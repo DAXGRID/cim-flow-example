8da3a1b6ad709062752645febd35d8f3a7c487cd
runeanielsen <runenielsen@runbox.com>
2025-04-14 09:32:02 +0200

    restructure and add delete example

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 Contains example scripts that showcases interactions with the CIM Flow platform.
 
-## How to upload
+## Examples using the PowerShell 7+
 
-### Example using the PowerShell 7+
+### How to upload
 
 ```sh
 pwsh ./upload.ps1 -Files /home/my_user/my_file.xml -Url "https://files.customer_name.cimflow.net/input" -Username "my_username" -Password "mypassword"
+```
+
+```sh
+pwsh ./delete.ps1 -Url "https://files.customer_name.cimflow.net/my_file.zip" -Username "my_username" -Password "mypassword"
 ```
---
+++ powershell-examples/delete.ps1
@@ -0,0 +1,23 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $Url,
+
+    [Parameter(Mandatory = $true)]
+    [string] $Username,
+
+    [Parameter(Mandatory = $true)]
+    [string] $Password
+)
+
+try {
+    $credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, (ConvertTo-SecureString $Password -AsPlainText -Force)
+
+    Write-Host "Deleteing file in path '$Url'."
+
+    $response = Invoke-WebRequest -Uri $url -Method Delete -Credential $credentials
+
+    Write-Host "Status Code: $($response.StatusCode)"
+}
+catch {
+    Write-Error "Error: $_"
+}
